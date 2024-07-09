4251381d7a6f3ca62dcd87756bc484b5211a6a70
rayzen2002 <emanuelzsmj@gmail.com>
2024-07-08 22:12:05 -0300

    req: To Create a pet you need to be logged in

--- api/src/http/controllers/pet/routes.ts
+++ api/src/http/controllers/pet/routes.ts
@@ -3,9 +3,14 @@ import { register } from './register-controller'
 import { details } from './details-controller'
 import { detailsByCity } from './details-by-city-controller'
 import { detailsWithFilters } from './details-with-filters-controller'
+import { verifyJWT } from '../../middlewares/verifyjwt'
 
 export async function petRoutes(app: FastifyInstance) {
-  app.post('/organization/:organizationId/pet', register)
+  app.post(
+    '/organization/:organizationId/pet',
+    { onRequest: [verifyJWT] },
+    register,
+  )
   app.get('/pets/:city/:petId', details)
   app.get('/pet/city/:city', detailsWithFilters)
   app.get('/pets/city/:city', detailsByCity)
